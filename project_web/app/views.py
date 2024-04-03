5fe4cef5e392b4f3bac4077dd97854f4b317e479
Luiz Santos <luizfernando.santos900@gmail.com>
2024-04-03 20:47:17 -0300

    second commit

--- project_web/app/views.py
+++ project_web/app/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
+
 # from django.views.decorators.cache import never_cache
+
 
 # @never_cache
 def home(request):
     cursos = [
         {
-            'nome': 'Tecnologia em Análise e Desenvolvimento de Sistemas',
-            'imagem': 'caminho/para/imagem1.jpg',
+            "nome": "Tecnologia em Análise e Desenvolvimento de Sistemas",
         },
         {
-            'nome': 'Redes de Computadores',
-            'imagem': 'caminho/para/imagem2.jpg',
+            "nome": "Redes de Computadores",
         },
         {
-            'nome': 'Sistemas de Informação',
-            'imagem': 'caminho/para/imagem3.jpg',
-        }
+            "nome": "Sistemas de Informação",
+        },
     ]
-    contexto = {
-        'cursos': cursos
-    }
-    return render(request, 'cursos.html', contexto)
+    contexto = {"cursos": cursos}
+    return render(request, "cursos.html", contexto)
--- project_web/project_web/urls.py
+++ project_web/project_web/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cursos/', include('app.urls')),
